524fba4ba9bdfcef2fcaf9f221942cc87654302b
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-01-30 13:27:15 +0200

    add fixtures

---
+++ src/events-fixtures.js
@@ -0,0 +1,25 @@
+const events = [
+  {
+    id: '0000-0000-0000-0001',
+    name: 'Daily Standup',
+    date: 'Mon',
+    time: '11:00',
+    members: [{ id: '0001' }, { id: '0002' }, { id: '0003' }, { id: '0004' }],
+  },
+  {
+    id: '0000-0000-0000-0002',
+    name: 'Planning session',
+    date: 'Wed',
+    time: '13:00',
+    members: [{ id: '0001' }, { id: '0002' }],
+  },
+  {
+    id: '0000-0000-0000-0003',
+    name: 'Retrospective',
+    date: 'Fri',
+    time: '17:00',
+    members: [{ id: '0003' }, { id: '0004' }],
+  },
+];
+
+export default events;
---
+++ src/teamMembers-fixtures.js
@@ -0,0 +1,8 @@
+const teamMembers = [
+  { id: '0001', name: 'Polina' },
+  { id: '0002', name: 'Maria' },
+  { id: '0003', name: 'Bob' },
+  { id: '0004', name: 'Alex' },
+];
+
+export default teamMembers;
