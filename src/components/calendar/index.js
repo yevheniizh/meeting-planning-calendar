37cf36911f380f596fe7de55f4a137ac0c1b68c9
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-02-14 22:38:06 +0200

    fix CreateEvent href

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -97,7 +97,7 @@ export default class Calendar {
   get getEventButton() {
     return `
     <div class='calendar__header_handling-newEventCreatingButton'>
-      <a href='/create-event'>
+      <a href='/meeting-planning-calendar/create-event'>
         <button
           type='submit'
           name='newEvent'
