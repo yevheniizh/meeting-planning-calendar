9b4281dd7b43a9253a0ffa6b1facf1cae5ca3a8b
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-03-09 18:26:13 +0200

    fix fixtures as mock data for tests

--- src/fixtures-meetings.js
+++ src/fixtures-meetings.js
@@ -1,39 +1,47 @@
-const meetings = [
+export const meetingsMock = [
   {
     id: '0000-0000-0000-0001',
-    name: 'Daily Standup',
-    day: 'Mon',
-    time: '11:00',
-    members: [{ id: '0001' }, { id: '0002' }, { id: '0003' }, { id: '0004' }],
+    data: {
+      name: 'Daily Standup',
+      day: 'Mon',
+      time: '11:00',
+      members: [{ id: '0001' }, { id: '0002' }, { id: '0003' }, { id: '0004' }],
+    },
   },
   {
     id: '0000-0000-0000-0002',
-    name: 'Annual planning session',
-    day: 'Wed',
-    time: '13:00',
-    members: [{ id: '0001' }, { id: '0002' }],
+    data: {
+      name: 'Annual planning session',
+      day: 'Wed',
+      time: '13:00',
+      members: [{ id: '0001' }, { id: '0002' }],
+    },
   },
   {
     id: '0000-0000-0000-0003',
-    name: 'Retrospective',
-    day: 'Fri',
-    time: '17:00',
-    members: [{ id: '0003' }, { id: '0004' }],
+    data: {
+      name: 'Retrospective',
+      day: 'Fri',
+      time: '17:00',
+      members: [{ id: '0003' }, { id: '0004' }],
+    },
   },
   {
     id: '0000-0000-0000-0004',
-    name: 'FE team sync',
-    day: 'Thu',
-    time: '17:00',
-    members: [{ id: '0004' }],
+    data: {
+      name: 'FE team sync',
+      day: 'Thu',
+      time: '17:00',
+      members: [{ id: '0004' }],
+    },
   },
   {
     id: '0000-0000-0000-0005',
-    name: 'Planning session',
-    day: 'Tue',
-    time: '10:00',
-    members: [{ id: '0001' }, { id: '0004' }],
+    data: {
+      name: 'Planning session',
+      day: 'Tue',
+      time: '10:00',
+      members: [{ id: '0001' }, { id: '0004' }],
+    },
   },
 ];
-
-export default meetings;
--- src/fixtures-members.js
+++ src/fixtures-members.js
@@ -1,19 +1,10 @@
-export const members = [
-  { id: '0001', name: 'Polina', rights: 'admin' },
-  { id: '0002', name: 'Maria', rights: 'user' },
-  { id: '0003', name: 'Bob', rights: 'user' },
-  { id: '0004', name: 'Alex', rights: 'user' },
-];
-
-// users template to send to server
-export const membersTemplate = [
-  { id: 0, data: { name: 'Polina', rights: 'admin' } },
-  { name: 'Polina', rights: 'admin' },
-  { name: 'Maria', rights: 'user' },
-  { name: 'Bob', rights: 'user' },
-  { name: 'Alex', rights: 'user' },
+export const membersMock = [
+  { id: '0001', data: { name: 'Polina', rights: 'admin' } },
+  { id: '0002', data: { name: 'Maria', rights: 'user' } },
+  { id: '0003', data: { name: 'Bob', rights: 'user' } },
+  { id: '0004', data: { name: 'Alex', rights: 'user' } },
 ];
 
 export const noMembersMock = [
-  { id: 0, data: { name: 'guest', rights: 'user' } },
+  { id: '0000', data: { name: 'guest', rights: 'user' } },
 ];
